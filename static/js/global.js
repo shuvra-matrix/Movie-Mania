cb070cb74cc740d3b5b3ab430c9834d0b275e85f
SHUVRA CHAKRABARTY <75852937+shuvra-matrix@users.noreply.github.com>
2023-05-22 03:01:07 +0530

    nav bar create done

---
+++ static/js/global.js
@@ -0,0 +1,12 @@
+"usar strict";
+
+const searchBox = document.querySelector(".search-box");
+console.log(searchBox);
+const searchToggler = document.querySelectorAll(".search-btn");
+
+for (tog of searchToggler) {
+  console.log(tog);
+  tog.addEventListener("click", () => {
+    searchBox.classList.toggle("active");
+  });
+}
