571dd24a9e62422f3a25c455112f25e03f2e06f2
SHUVRA CHAKRABARTY <75852937+shuvra-matrix@users.noreply.github.com>
2023-05-22 04:20:21 +0530

    movies api fetch

--- static/js/global.js
+++ static/js/global.js
@@ -10,3 +10,26 @@ for (tog of searchToggler) {
     searchBox.classList.toggle("active");
   });
 }
+
+const settings = {
+  async: true,
+  crossDomain: true,
+  url: "https://imdb-top-100-movies.p.rapidapi.com/",
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "53db47703bmsh43337a6ff98140ep1d9019jsnfa4b3f6ce92b",
+    "X-RapidAPI-Host": "imdb-top-100-movies.p.rapidapi.com",
+  },
+};
+
+$.ajax(settings).done(function (response) {
+  console.log(response);
+  response.forEach((res) => {
+    if (res.title !== "Come and See") {
+      let titles = res.title.slice(0, 16) + "...";
+      $(".popular-show").append(
+        `<div class="image"> <img src="${res.image}" alt="${res.title}"> <div class="title"><h3>${titles}</h3></div>  </div>`
+      );
+    }
+  });
+});
---
+++ static/js/tempCodeRunnerFile.js
@@ -0,0 +1,10 @@
+const searchBox = document.querySelector(".search-box");
+// console.log(searchBox);
+// const searchToggler = document.querySelectorAll(".search-btn");
+
+// for (tog of searchToggler) {
+//   console.log(tog);
+//   tog.addEventListener("click", () => {
+//     searchBox.classList.toggle("active");
+//   });
+// }
